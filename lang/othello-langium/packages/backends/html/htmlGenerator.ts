f6df4c0bca46beafd017cd702a9a9ce047650866
Fir3n0x <corentinmahieu50@gmail.com>
2025-11-07 11:28:27 +0100

    ia minmax premice

--- lang/othello-langium/packages/backends/html/htmlGenerator.ts
+++ lang/othello-langium/packages/backends/html/htmlGenerator.ts
@@ -241,7 +241,7 @@ html += `
                 board: board,
                 player: currentPlayer
             };
-            fetch('http://localhost:5000', {
+            fetch('http://127.0.0.1:5000/move', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify(payload)
